76c537fb0b730c421a24bdfce9deeffc9bd9460e
mingj7235 <joshuara7235@gmail.com>
2024-07-25 01:19:33 +0900

    feat : 잔액 조회에 spin lock 을 구현한다.

--- src/main/kotlin/com/hhplus/concert/business/application/service/BalanceService.kt
+++ src/main/kotlin/com/hhplus/concert/business/application/service/BalanceService.kt
@@ -2,10 +2,10 @@ package com.hhplus.concert.business.application.service
 
 import com.hhplus.concert.business.application.dto.BalanceServiceDto
 import com.hhplus.concert.business.domain.manager.BalanceManager
+import com.hhplus.concert.common.annotation.DistributedSpinLock
 import com.hhplus.concert.common.error.code.ErrorCode
 import com.hhplus.concert.common.error.exception.BusinessException
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 
 @Service
 class BalanceService(
@@ -17,13 +17,26 @@ class BalanceService(
      * - user 는 존재하지만 balance 가 없다면 생성하고 충전한다.
      * - balance 가 존재한다면, 현재 금액에 요청된 금액을 더한다.
      */
-    @Transactional
+
+    @DistributedSpinLock(
+        key = "'user:' + #userId",
+    )
     fun recharge(
         userId: Long,
         amount: Long,
     ): BalanceServiceDto.Detail {
         if (amount < 0) throw BusinessException.BadRequest(ErrorCode.Balance.BAD_RECHARGE_REQUEST)
 
+        return rechargeWithSimpleLock(
+            userId = userId,
+            amount = amount,
+        )
+    }
+
+    fun rechargeWithSimpleLock(
+        userId: Long,
+        amount: Long,
+    ): BalanceServiceDto.Detail {
         val rechargedBalance =
             balanceManager.updateAmount(
                 userId = userId,
