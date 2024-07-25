dcc710a6f4fec1dc2de5a1ba9b222cd4c6f1f72a
mingj7235 <joshuara7235@gmail.com>
2024-07-26 00:17:01 +0900

    test : 낙관적락은 동시에 많은 요청이 오는 경우에는 적합하지 않다.

--- src/test/kotlin/com/hhplus/concert/application/facade/integration/BalanceServiceConcurrencyTest.kt
+++ src/test/kotlin/com/hhplus/concert/application/facade/integration/BalanceServiceConcurrencyTest.kt
@@ -5,7 +5,6 @@ import com.hhplus.concert.business.domain.entity.Balance
 import com.hhplus.concert.business.domain.entity.User
 import com.hhplus.concert.business.domain.repository.BalanceRepository
 import com.hhplus.concert.business.domain.repository.UserRepository
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -13,7 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest
 import java.time.LocalDateTime
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.Executors
-import java.util.concurrent.atomic.AtomicInteger
 
 @SpringBootTest
 class BalanceServiceConcurrencyTest {
@@ -30,7 +28,7 @@ class BalanceServiceConcurrencyTest {
      * 동시에 많은 요청이 들어온다면 낙관적락은 적합하지 않다.
      */
     @Test
-    fun `1000개의 동시 충전 요청이 오면 수차적으로 모두 성공해야 한다`() {
+    fun `100 회의 동시 충전 요청이 오면 1회만 성공해야 한다`() {
         // Given
         val user = userRepository.save(User(name = "Test User"))
         balanceRepository.save(
@@ -62,6 +60,6 @@ class BalanceServiceConcurrencyTest {
 
         // Then
         val finalBalance = balanceRepository.findByUserId(user.id)
-        assertEquals(101000L, finalBalance?.amount, "최종 잔액은 101000이어야 합니다.")
+        assertEquals(200L, finalBalance?.amount, "최종 잔액은 200이어야 합니다.")
     }
 }
