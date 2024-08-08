c45c20b0bfd0e7a220f75d0573ff519fb97e67da
mingj7235 <joshuara7235@gmail.com>
2024-08-08 21:51:58 +0900

    chore : 어노테이션 수정

--- src/main/kotlin/com/hhplus/concert/ConcertApplication.kt
+++ src/main/kotlin/com/hhplus/concert/ConcertApplication.kt
@@ -2,10 +2,12 @@ package com.hhplus.concert
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 import org.springframework.scheduling.annotation.EnableScheduling
 
-@SpringBootApplication
+@EnableCaching
 @EnableScheduling
+@SpringBootApplication
 class ConcertApplication
 
 fun main(args: Array<String>) {
