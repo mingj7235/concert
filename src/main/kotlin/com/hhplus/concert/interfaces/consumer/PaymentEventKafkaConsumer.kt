c4381844e4932ef9a08b3f65d2ae7c5bfcc4799a
mingj7235 <joshuara7235@gmail.com>
2024-08-15 13:13:40 +0900

    feat : Kafka Listener 와 Producer 를 개발한다.

---
+++ src/main/kotlin/com/hhplus/concert/interfaces/consumer/PaymentEventKafkaConsumer.kt
@@ -0,0 +1,21 @@
+package com.hhplus.concert.interfaces.consumer
+
+import com.hhplus.concert.business.application.service.PaymentService
+import org.slf4j.LoggerFactory
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.scheduling.annotation.Async
+import org.springframework.stereotype.Component
+
+@Component
+class PaymentEventKafkaConsumer(
+    private val paymentService: PaymentService,
+) {
+    @Async
+    @KafkaListener(topics = ["payment-event"], groupId = "concert")
+    fun handleSendMessageKafkaEvent(paymentId: String) {
+        logger.info("KafkaEvent received")
+        paymentService.sendPaymentEventMessage(paymentId.toLong())
+    }
+
+    private val logger = LoggerFactory.getLogger(this::class.java)
+}
